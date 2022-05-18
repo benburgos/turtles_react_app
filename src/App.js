ef78f41d928a166b641f04b5635072bcf8eae4af
Ben <ben.a.burgos@gmail.com>
2022-05-17 22:23:02 -0700

    adds list of documents from db

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [turtle, setTurtle] = useState([]);
+
+  useEffect(() => {
+    async function getTurtles() {
+      const response = await fetch(
+        'https://bb-sei-turtle-api.herokuapp.com/turtles'
+      );
+      const data = await response.json();
+      setTurtle(data);
+    }
+    getTurtles();
+  }, []);
+
   return (
     <div className="App">
       <h1>It me!</h1>
+      {turtle.map((turtle, index) => {
+        return (
+          <div>
+            {turtle.name}, {turtle.role}
+          </div>
+        );
+      })}
     </div>
   );
 }
